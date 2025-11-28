c69e83918a31de225eedeb1a15eb9c549a1512c7
Shohanur Rahman <shohanurr490@gmail.com>
2025-11-28 18:12:34 +0600

    added multi file model

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, env } from 'prisma/config';
 type Env = { DATABASE_URL: string };
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: 'prisma/schema',
   migrations: { path: 'prisma/migrations' },
   datasource: {
     url: env<Env>('DATABASE_URL'),
--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,10 @@ export class AppService {
   constructor(private readonly db: DatabaseService) {}
 
   async getHello(): Promise<any> {
+    await this.db.user.createMany({
+      data: [{ name: 'dsf', email: 'dhwsdh' }],
+    });
+
     return await this.db.user.findMany();
   }
 }
